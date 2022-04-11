699b66344cc4ed63a67869727f80d8f9ef2730dc
Maksim Basov <maksimbasov1992@mail.ru>
2022-04-11 16:17:10 +0300

    feat: header-black done

---
+++ Scripts/main.js
@@ -0,0 +1,39 @@
+// const selector = document.querySelector('.header__selector');
+
+// selector.addEventListener('click', function() {
+//     selector.classList.toggle('header__selector_direction')
+// })
+
+// document.addEventListener('click', function(e) {
+//     if (e.target.id != 'selector') {
+//         selector.classList.remove('header__selector_direction')
+//     }
+//   });
+
+jQuery(($) => {
+    $('.select').on('click', '.select__head', function () {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).next().fadeOut();
+        } else {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+            $(this).addClass('open');
+            $(this).next().fadeIn();
+        }
+    });
+
+    $('.select').on('click', '.select__item', function () {
+        $('.select__head').removeClass('open');
+        $(this).parent().fadeOut();
+        $(this).parent().prev().text($(this).text());
+        $(this).parent().prev().prev().val($(this).text());
+    });
+
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.select').length) {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+        }
+    });
+});
