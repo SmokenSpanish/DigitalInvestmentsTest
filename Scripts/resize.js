bc110a94b9dd01fdfc7dea8669df2ad27f8f7229
Maksim Basov <maksimbasov1992@mail.ru>
2022-04-13 10:47:43 +0300

    fix: burger fixed

--- Scripts/resize.js
+++ Scripts/resize.js
@@ -6,13 +6,13 @@ function resizeBlock(){
         $('.header__searcher img').attr('src', 'Images/Search2.svg')
         $('.menu-burger__body').append($('#header__nav_type-white'));
         $('.menu-burger__body').append($('#header__nav_type-black'));
-    } if(window.matchMedia("screen and (max-width: 634px)").matches) {
-        $('.menu-burger__title-container').append($('.header__select'));
-        $('.menu-burger__title-container').append($('.header__searcher'));
     } else {
         $('.header__nav-container').prepend($('.header__select'));
         $('.header__type_black').append($('.header__searcher'));
         $('.header__searcher img').attr('src', 'Images/Search1.svg')
+    } if(window.matchMedia("screen and (max-width: 634px)").matches) {
+        $('.menu-burger__title-container').append($('.header__select'));
+        $('.menu-burger__title-container').append($('.header__searcher'));
     } 
 }
 
