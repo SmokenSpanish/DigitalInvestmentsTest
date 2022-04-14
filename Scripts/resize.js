e9e9e37eef1d5a1a710668158cd8ede09585095c
Maksim Basov <maksimbasov1992@mail.ru>
2022-04-14 11:37:29 +0300

    fix: resize.js fixed

--- Scripts/resize.js
+++ Scripts/resize.js
@@ -8,15 +8,20 @@ function resizeBlock(){
         $('.menu-burger__body').append($('#header__nav_type-white'));
         $('.menu-burger__body').append($('#header__nav_type-black'));
     } else {
+        $('.flex-wrapper').prepend($('.social'));
         $('.header__nav-container').prepend($('.header__select'));
+        $('.select__list').addClass('shadow');
+        $('.header__nav-container').append($('#header__nav_type-black'));
+        $('.header__type_white').prepend($('#header__nav_type-white'));
         $('.header__type_black').append($('.header__searcher'));
-        $('.header__searcher img').attr('src', 'Images/Search1.svg')
+        $('.header__searcher img').attr('src', 'Images/Search1.svg');
+        
     } if(window.matchMedia("screen and (max-width: 634px)").matches) {
         $('.menu-burger__title-container').append($('.header__select'));
         $('.menu-burger__title-container').append($('.header__searcher'));
         $('.header__type_white-container').append($('.social'));
         $('.menu-burger__body').append($('.header__type_white-container'));
-    } 
+    }
 }
 
 $(window).resize(function() {
